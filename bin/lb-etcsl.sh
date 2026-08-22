5c852be0e172471127ba729842d4f0dc29fd283e
Steve Tinney <stinney@upenn.edu>
2026-08-22 22:47:27 +0100

    lb very basic print routine and add line-by-line gen to driver script

--- bin/lb-etcsl.sh
+++ bin/lb-etcsl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for a in lbpp/*.tsv ; do
     e=`basename $a .tsv`.err
+    l=`basename $a .tsv`.lbl
     echo $a
-    src/lb $a 2>lbpp/$e
+    src/lb $a >lbl/$l 2>lbpp/$e
 done
--- src/lb_print.c
+++ src/lb_print.c
@@ -2,6 +2,63 @@
 #include "lb.h"
 
 void
+lbp_print_seg(FILE *fp, Seg *s)
+{
+  if (s->o)
+    {
+      const char *o = s->o;
+      while (isspace(*o))
+	++o;
+      if ('0' == s->b)
+	{
+	  int len = s->c - s->o;
+	  char buf[len+1];
+	  strncpy(buf, s->o, len);
+	  buf[len] = '\0';
+	  fprintf(fp, "($%s$)",buf);
+	}
+      else
+	{
+	  while (o < s->c)
+	    {
+	      if (CTRL_Q == *o)
+		fputs(" (?)", fp);
+	      else
+		fputc(*o, fp);
+	      ++o;
+	    }
+	}
+    }
+  fputc(' ', fp);
+}
+
+void
+lbp_seg(FILE *fp, Seg *s)
+{
+  if (s->with)
+    return;
+  
+  if (s->label)
+    fprintf(fp, "%s.\t", s->label);
+  lbp_print_seg(fp, s);
+  while ((s = s->next))
+    lbp_print_seg(fp, s);
+  fputc('\n', fp);
+}
+
+void
+lbp_par(FILE *fp, Par *p)
+{
+  int i;
+  for (i = 0; i < p->nsegs; ++i)
+    lbp_seg(fp, p->segs[i]);
+}
+
+void
 lb_print(FILE *fp, Tra *t)
 {
+  fprintf(fp, "&%s\n", t->Q);
+  int i;
+  for (i = 0; i < t->npars; ++i)
+    lbp_par(fp, &t->pars[i]);
 }
